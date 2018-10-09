c27d69ca5dbd5a748f0018c83e105f29c8d660b3
Morten <morto1995@hotmail.com>
2018-10-09 11:10:35 +0200

    code optimized

--- kortSpilConsole/UnoGame.cs
+++ kortSpilConsole/UnoGame.cs
@@ -49,56 +49,28 @@ namespace kortSpilConsole
                     if (currentPlayer.Hand[i-1].Value == "+2")
                     {
                         Console.WriteLine("Næste spiller "+ players[players.IndexOf(currentPlayer) + 1].name + " trækker 2 kort");
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
+                        for (int j = 0; j < 2; j++) players[players.IndexOf(currentPlayer) + 1].DrawCard();
                     }
                     else if (currentPlayer.Hand[i - 1].Value == "change +4")
                     {
                         Console.WriteLine("Næste spiller " + players[players.IndexOf(currentPlayer) + 1].name + " trækker 4 kort");
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
-                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
+                        for (int j = 0; j < 4; j++) players[players.IndexOf(currentPlayer) + 1].DrawCard();
 
                         Console.WriteLine("Hvilken farve vil du have? (b)lue, (g)reen, (y)ellow eller (r)ed");
                         string newColor = Console.ReadLine();
-                        if (newColor == "b")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "blue";
-                        }
-                        else if (newColor == "g")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "green";
-                        }
-                        else if (newColor == "y")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "yellow";
-                        }
-                        else if (newColor == "r")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "red";
-                        }
+                        if (newColor == "b")currentPlayer.Hand[i - 1].Color = "blue";
+                        else if (newColor == "g")currentPlayer.Hand[i - 1].Color = "green";
+                        else if (newColor == "y")currentPlayer.Hand[i - 1].Color = "yellow";
+                        else if (newColor == "r")currentPlayer.Hand[i - 1].Color = "red";
                     }
                     else if (currentPlayer.Hand[i - 1].Value == "change")
                     {
                         Console.WriteLine("Hvilken farve vil du have? (b)lue, (g)reen, (y)ellow eller (r)ed");
                         string newColor = Console.ReadLine();
-                        if (newColor == "b")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "blue";
-                        }
-                        else if (newColor == "g")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "green";
-                        }
-                        else if (newColor == "y")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "yellow";
-                        }
-                        else if (newColor == "r")
-                        {
-                            currentPlayer.Hand[i - 1].Color = "red";
-                        }
+                        if (newColor == "b") currentPlayer.Hand[i - 1].Color = "blue";
+                        else if (newColor == "g") currentPlayer.Hand[i - 1].Color = "green";
+                        else if (newColor == "y") currentPlayer.Hand[i - 1].Color = "yellow";
+                        else if (newColor == "r") currentPlayer.Hand[i - 1].Color = "red";
                     }
 
                     currentPlayer.Hand.Remove(currentPlayer.Hand[i - 1]);
