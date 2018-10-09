6cd2bfdca29b946f9f653547876f1a0ce00d136c
Morten <morto1995@hotmail.com>
2018-10-09 10:48:48 +0200

    +2 rules added

--- kortSpilConsole/UnoGame.cs
+++ kortSpilConsole/UnoGame.cs
@@ -22,8 +22,11 @@ namespace kortSpilConsole
             players.Add(new Player("Beta", this));
             currentPlayer = players.First();
             //del kort ud til spiller 1
-            players[0].DrawCard(6);
+            players[0].DrawCard(3);
             players[0].DebugDrawCard("red", "+2");
+            players[0].DebugDrawCard("green", "+2");
+            players[0].DebugDrawCard("blue", "+2");
+            players[0].DebugDrawCard("yellow", "+2");
             //del 7 kort ud til resten af spillerne
             for (int i = 1; i < players.Count; i++)
             {
@@ -45,7 +48,14 @@ namespace kortSpilConsole
                 //TODO prøv at 'spille' det valgte kort til bunken
                 if (deck.playCard(currentPlayer.Hand[i - 1]))
                 {
+                    if (currentPlayer.Hand[i-1].Value == "+2")
+                    {
+                        Console.WriteLine("Næste spiller trækker 2 kort");
+                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
+                        players[players.IndexOf(currentPlayer) + 1].DrawCard();
+                    }
                     currentPlayer.Hand.Remove(currentPlayer.Hand[i - 1]);
+                    
                 }
                 else
                 {
