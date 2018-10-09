90d35637010a878a8332303d20ae3115225381c9
laptop\Ebbe Vang <ev@evang.dk>
2018-10-09 08:46:43 +0200

    changed players to list of players
    game now support n players instead of just 2!

--- kortSpilConsole/UnoGame.cs
+++ kortSpilConsole/UnoGame.cs
@@ -10,22 +10,20 @@ namespace kortSpilConsole
     class UnoGame
     {
         public Deck deck;
-        public Player player1;
-        public Player player2;
-
+        List<Player> players = new List<Player>();
+        private Player currentPlayer;
         private bool gameover = false;
 
         public UnoGame()
         {
             deck = new Deck(this);
-            player1 = new Player("Alfa", this);
-            player2 = new Player("Beta", this);
-
-            Player currentPlayer = player1;
-
+            
+            players.Add(new Player("Alfa", this));
+            players.Add(new Player("Beta", this));
+            currentPlayer = players.First();
             //del 7 kort ud til spillerne
-            player1.DrawCard(7);
-            player2.DrawCard(7);
+            players[0].DrawCard(7);
+            players[1].DrawCard(7);
 
             while (gameover != true)
             {
@@ -42,17 +40,25 @@ namespace kortSpilConsole
                 //TODO prøv at 'spille' det valgte kort til bunken
                 if (deck.playCard(currentPlayer.Hand[i - 1]))
                 {
-                    currentPlayer.Hand.Remove(player1.Hand[i - 1]);
+                    currentPlayer.Hand.Remove(currentPlayer.Hand[i - 1]);
 
                 }
 
-                if (currentPlayer.Equals(player1))
-                {
-                    currentPlayer = player2;
-                }
-                else currentPlayer = player1;
+                nextPlayer();
             }
         }
 
+        private void nextPlayer()
+        {
+            if (currentPlayer == players.Last())
+            {
+                currentPlayer = players.First();
+            }
+            else
+            {
+                int currentPlayerPosition = players.IndexOf(currentPlayer);
+                currentPlayer = players[currentPlayerPosition + 1];
+            }
+        }
     }
 }
